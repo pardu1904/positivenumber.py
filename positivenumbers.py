7d7147d6021a3499b9d4bb2caa0e55776816ad3b
pardu1904 <67218432+pardu1904@users.noreply.github.com>
2020-06-21 12:28:51 +0530

    the code is used to print the positive numbers from a given list using python

---
+++ positivenumbers.py
@@ -0,0 +1,10 @@
+  
+list1 = [12,-7,5,64,-14] 
+for num in list1: 
+    if num >= 0: 
+       print(num, end = " is the positive number in list1\n") 
+        
+list2 = [12,14,-95,3] 
+for num in list2: 
+    if num >= 0: 
+       print(num, end = " is the positive number in list2\n")  
